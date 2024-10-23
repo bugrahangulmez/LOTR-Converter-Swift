3cdbb49be2ca15a700b68c71ead80f785bdeb24b
bugrahan gulmez <114365074+bugrahangulmez@users.noreply.github.com>
2024-10-24 00:41:00 +0300

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Bugrahan on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
